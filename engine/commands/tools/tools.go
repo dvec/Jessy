b850afab29492c7c5771b1e7887b1558fdf1fe25
Anton Ugryumov <ugryumov124@gmail.com>
2017-05-18 16:30:13 +0400

    Improved code readability

--- engine/commands/tools/tools.go
+++ engine/commands/tools/tools.go
@@ -6,8 +6,36 @@ import (
 	"main/engine/cache"
 )
 
+const (
+	//GET_RANDOM
+	startNum	= 50
+
+	//CHECK_DATA
+	integerTag 	= "i"
+	stringTag	= "s"
+	anythingTag	= "*"
+
+	//GET_HELP
+	ponyDescription	= "%v | %v \n"
+	simpleAnswer	= "Список моих команд: \n%v" +
+		" Вы можете посмотреть справку по любой из них, набрав:" +
+		" \nпомощь [название команды]"
+	complexAnswer	= `Справка по команде "%v": %v %v`
+	sampleIntroText	= "Примеры использования: \n"
+	sampleTemplate	= "USER> %v\nJESSY> %v"
+	noHelpError	= "Нет справки по такой команде"
+)
+
+var emojiDict = map[string]string{
+	"ready": "&#128215;",
+	"test": "&#128217;",
+	"error": "&#128213;",
+	"dev": "&#128216;",
+
+}
+
 func GetRandomNum(text string) int {
-	out := 50
+	out := startNum
 	for _, char := range text {
 		out += int(char)
 	}
@@ -22,12 +50,14 @@ func CheckData(args []string, template []string) bool {
 	l: for index, word := range args {
 		if index < len(template) {
 			switch template[index] {
-			case "i":
+			case integerTag:
 				_, err := strconv.ParseInt(word, 10, 64)
 				if err != nil {
 					return false
 				}
-			case "*":
+			case stringTag:
+				//TODO ADD
+			case anythingTag:
 				break l
 			}
 		}
@@ -39,21 +69,12 @@ func CheckData(args []string, template []string) bool {
 func GetHelp(name string, cache cache.HelpCache) string {
 	if name == "" {
 		var commandList string
-		var emojiDict = map[string]string{
-			"ready": "&#128215;",
-			"test": "&#128217;",
-			"error": "&#128213;",
-			"dev": "&#128216;",
-
-		}
 		cache.Lock()
 		defer cache.Unlock()
 		for _, command := range cache.Data.HelpList {
-			commandList += fmt.Sprintf("%v | %v \n", emojiDict[command.State], command.Name)
+			commandList += fmt.Sprintf(ponyDescription, emojiDict[command.State], command.Name)
 		}
-		return fmt.Sprintf("Список моих команд: \n%v" +
-			" Вы можете посмотреть справку по любой из них, набрав:" +
-			" \nпомощь [название команды]", commandList)
+		return fmt.Sprintf(simpleAnswer, commandList)
 	}
 	cache.Lock()
 	defer cache.Unlock()
@@ -61,16 +82,15 @@ func GetHelp(name string, cache cache.HelpCache) string {
 		if command.Name == name {
 			samples := ""
 			if command.Samples != nil {
-				samples = "Примеры использования: \n"
+				samples = sampleIntroText
 				for _, sample := range command.Samples {
-					samples += fmt.Sprintf("USER> %v\nJESSY> %v", sample.Body, sample.Out)
+					samples += fmt.Sprintf(sampleTemplate, sample.Body, sample.Out)
 				}
 			}
-			return fmt.Sprintf("Справка по команде "+
-				"\"%v\": %v %v", name, command.Description, samples)
+			return fmt.Sprintf(complexAnswer, name, command.Description, samples)
 		}
 	}
-	return "Нет справки по такой команде"
+	return noHelpError
 }
 
 func Contains(arr []string, value string) bool {
